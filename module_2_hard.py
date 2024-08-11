e7dc9663f117ce463974a33f65b3847a52fb7e36
Yed Makarov <myed@list.ru>
2024-08-11 23:25:35 +0500

    Urban-University homeworks

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,19 +1,13 @@
 # Задание "Слишком древний шифр"
 
-str_result = ""
 
 num = int(input("Введите число в диапазоне 3 - 20 :"))
 if num >= 3 and num <= 20:
     pass
-else:
-    print("Вы ввели неправильное число")
 
 for i in range(num):
     i += 1
     for j in range(1, num):
         if i != j:
             if num % (i + j) == 0:
-                if i < j:
-                    str_result += str(i) + str(j)
 
-print(str_result)
---
+++ module_3_hard.py
@@ -0,0 +1,19 @@
+# Задание "Раз, два, три, четыре, пять .... Это не всё?"
+
+data_structure = [
+  [1, 2, 3],
+  {'a': 4, 'b': 5},
+  (6, {'cube': 7, 'drum': 8}),
+  "Hello",
+  ((), [{(2, 'Urban', ('Urban2', 35))}])
+]
+
+def calculate_structure_sum(*atrgs):
+    pass
+
+
+
+
+# Main
+result = calculate_structure_sum(data_structure)
+print(result)
