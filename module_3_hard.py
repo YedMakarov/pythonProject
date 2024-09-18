574d0cd38b49503d9c46b951f2511b1373baf7ab
Yed Makarov <myed@list.ru>
2024-09-19 01:48:31 +0500

    Urban-University homeworks

--- module_3_hard.py
+++ module_3_hard.py
@@ -1,6 +1,6 @@
 # Задание "Раз, два, три, четыре, пять .... Это не всё?"
 
-data_structure = [
+data_struct = [
     [1, 2, 3],
     {'a': 4, 'b': 5},
     (6, {'cube': 7, 'drum': 8}),
@@ -12,16 +12,16 @@ list_instances = []
 sum = 0
 
 
-def calculate_structure_sum(params):
-    global sum
+def calculate_sum_(params):
+    summa = 0
 
-    for i in range(len(data_structure)):
+    for i in range(len(data_struct)):
         # print(params[i], type(params[i]))
 
         if isinstance(params[i], list):
             # print(params[i])
             for j in range(len(params[i])):
-                sum += int(params[i][j])
+                summa += int(params[i][j])
 
         elif isinstance(params[i], dict):
             print(params[i])
@@ -33,11 +33,27 @@ def calculate_structure_sum(params):
 
         elif isinstance(params[i], str):
             # print(params[i])
-            sum += len(params[i])
+            summa += len(params[i])
 
     print()
-    return sum
+    return summa
+
+
+def calculate_sum(params):
+    summa = 0
+    if isinstance(params, int):
+        summa += params
+    elif isinstance(params, str):
+        summa += len(params)
+    elif isinstance(params, (list, tuple, set)):
+        for item in params:
+            summa += calculate_sum(item)
+    elif isinstance(params, dict):
+        for key, value in params.items():
+            summa += calculate_sum(key) + calculate_sum(value)
+    return summa
 
 
 # Main
-print(f"Результат: {calculate_structure_sum(data_structure)}")
+print(f"Результат: {calculate_sum(data_struct)}")
+#print(f"Результат: {calculate_sum_(data_struct)}")
