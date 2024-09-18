d228e5453a5f1827a222351238746fe06587ee84
Yed Makarov <myed@list.ru>
2024-09-19 02:02:01 +0500

    Urban-University homeworks

---
+++ module_7/module_7_2.py
@@ -0,0 +1,21 @@
+# Задача "Записать и запомнить"
+
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+    ]
+
+
+def custom_write(file_name, strings):
+
+    pass
+
+
+
+# Main
+
+result = custom_write('test.txt', info)
+for elem in result.items():
+  print(elem)
