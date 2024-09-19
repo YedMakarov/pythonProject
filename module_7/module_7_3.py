9de9863adfe61459da22dd48050a45de5f6b9f56
Yed Makarov <myed@list.ru>
2024-09-20 00:17:52 +0500

    Urban-University homeworks

---
+++ module_7/module_7_3.py
@@ -0,0 +1,16 @@
+# Задача "Найдёт везде"
+
+class WordsFinder:
+    pass
+
+
+
+
+
+
+# Main
+
+finder2 = WordsFinder('test_file.txt')
+print(finder2.get_all_words()) # Все слова
+print(finder2.find('TEXT')) # 3 слово по счёту
+print(finder2.count('teXT')) # 4 слова teXT в тексте всего
