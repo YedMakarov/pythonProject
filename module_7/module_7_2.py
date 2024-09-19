25d2c5df06614e2111996508813096064325b4a6
Yed Makarov <myed@list.ru>
2024-09-19 23:38:28 +0500

    Urban-University homeworks

--- module_7/module_7_2.py
+++ module_7/module_7_2.py
@@ -5,17 +5,28 @@ info = [
     'Используйте кодировку utf-8.',
     'Because there are 2 languages!',
     'Спасибо!'
-    ]
+]
 
 
 def custom_write(file_name, strings):
+    open(file_name, 'w').close()
 
-    pass
+    strings_positions = {}
 
+    file = open(file_name, 'a', encoding='utf-8')
+
+    for i in range(len(info)):
+        file_data = str(info[i])
+        d_key = (int(f"{str(i + 1)}"), int(f"{file.tell()}"))
+        d_value = f"{file_data}"
+        strings_positions[d_key] = d_value
+        file.write(f"{file_data}\n")
+    file.close()
+
+    return strings_positions
 
 
 # Main
-
 result = custom_write('test.txt', info)
 for elem in result.items():
-  print(elem)
+    print(elem)
