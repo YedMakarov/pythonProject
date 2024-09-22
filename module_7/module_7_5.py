8974cca6e855b8b58cfd1b3c84930a0a236a1931
Yed Makarov <myed@list.ru>
2024-09-22 11:56:21 +0500

    Urban-University homeworks

---
+++ module_7/module_7_5.py
@@ -0,0 +1,28 @@
+# "Файлы в операционной системе"
+
+import os
+import time
+
+# Main
+for root, dirs, files in os.walk('./'):
+    print("Родительский католог:")
+    print(root)
+    print()
+
+    print("Директории в родительском катологе:")
+    for name in dirs:
+        print(dirs)
+        print()
+
+    print("Файлфы в родительском катологе:")
+    for name in files:
+        filepath = root + "/" + name
+        filesize = os.path.getsize(filepath)
+        filetime = os.path.getmtime(filepath)
+        formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
+        parent_dir = os.path.dirname(filepath)
+
+        print(f'Обнаружен файл: {name}, Путь: {filepath}, Размер: {filesize} байт, Время изменения:'
+              f'{formatted_time}, Родительская директория: {parent_dir}')
+
+    print()
