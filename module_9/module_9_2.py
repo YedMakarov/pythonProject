24feaefe261f6395420e8e1cf7c4e446f1214c87
Yed Makarov <myed@list.ru>
2024-10-03 02:19:02 +0500

    Urban-University homeworks

---
+++ module_9/module_9_2.py
@@ -0,0 +1,16 @@
+# Задача - "Списковые, словарные сборки".
+
+first_strings = ['Elon', 'Musk', 'Programmer', 'Monitors', 'Variable']
+second_strings = ['Task', 'Git', 'Comprehension', 'Java', 'Computer', 'Assembler']
+
+first_result = [len(x) for x in first_strings if len(x) > 4]
+second_result = [(x,y) if len(x) == len(y) else """""" for x in first_strings for y in second_strings]
+# third_result =
+
+
+# Main
+
+print(first_result)
+print(second_result)
+# print(third_result)
+
