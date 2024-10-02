7ea8547276be8e7d0b7a7d42bd3a839577504de0
Yed Makarov <myed@list.ru>
2024-10-03 02:31:25 +0500

    Urban-University homeworks

--- module_9/module_9_2.py
+++ module_9/module_9_2.py
@@ -4,14 +4,11 @@ first_strings = ['Elon', 'Musk', 'Programmer', 'Monitors', 'Variable']
 second_strings = ['Task', 'Git', 'Comprehension', 'Java', 'Computer', 'Assembler']
 
 first_result = [len(x) for x in first_strings if len(x) > 4]
-# second_result = [(x,y) if len(x) == len(y) else """""" for x in first_strings for y in second_strings]
-second_result = [(x,y)  for x in first_strings for y in second_strings if len(x) == len(y)]
-# third_result =
-
+second_result = [(x, y) for x in first_strings for y in second_strings if len(x) == len(y)]
+third_result = {x: len(x) for x in (first_strings + second_strings) if not len(x) % 2}
 
 # Main
 
 print(first_result)
 print(second_result)
-# print(third_result)
-
+print(third_result)
