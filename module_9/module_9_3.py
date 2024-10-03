0702f2b2f0dedae4cc184bfe78a0ca1e8d500c9c
Yed Makarov <myed@list.ru>
2024-10-03 23:13:56 +0500

    Urban-University homeworks

--- module_9/module_9_3.py
+++ module_9/module_9_3.py
@@ -3,9 +3,9 @@
 first = ['Strings', 'Student', 'Computers']
 second = ['Строка', 'Урбан', 'Компьютер']
 
-# first_result = (len(x)-len(y) for x )
-second_result = (True if x == y else False for x in range(len(first)) for y in range(len(second)))
+first_result = (len(x) - len(y) for x, y in zip(first, second) if len(x) != len(y))
+second_result = (True if len(first[x]) == len(second[x]) else False for x in range(len(first)))
 
 # Main
-# print(list(first_result))
+print(list(first_result))
 print(list(second_result))
