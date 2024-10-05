0155c2555f19dd7218450baf16facdedfc06a15a
Yed Makarov <myed@list.ru>
2024-10-05 13:20:01 +0500

    Urban-University homeworks

--- module_9/module_9_5.py
+++ module_9/module_9_5.py
@@ -14,6 +14,7 @@ class Iterator:
 
     def __iter__(self):
         # Задать начальное значение указателя
+        # self.pointer = self.start
         self.pointer = self.start
         return self
 
