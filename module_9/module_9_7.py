ed40d184b8022222f9f7acce73ed6cef235f67e1
Yed Makarov <myed@list.ru>
2024-10-09 00:17:35 +0500

    Urban-University homeworks

---
+++ module_9/module_9_7.py
@@ -0,0 +1,22 @@
+# Задача по теме "Декораторы".
+
+def is_prime(func):
+    # def wrapper(*args):
+    #     result = func(*args)
+    # return wrapper
+    return func
+
+@is_prime
+def sum_three(*args: int):
+    result = 0
+
+    for i in args:
+        result += i
+    return result
+
+
+# Main
+result = sum_three(2, 3, 6)
+print(result)
+result = sum_three(2, 3, 7)
+print(result)
