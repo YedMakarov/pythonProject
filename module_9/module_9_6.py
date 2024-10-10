9462207b3c812e40c290cf5f0faec9bdfccc3381
Yed Makarov <myed@list.ru>
2024-10-10 20:11:11 +0500

    Urban-University homeworks

--- module_9/module_9_6.py
+++ module_9/module_9_6.py
@@ -1,19 +1,15 @@
 # Задача по теме "Генераторы".
 
 # def all_variants(text):
-#     for j in text:
-#         yield j
-
-
-# def all_variants(text):
 #     for i in range(len(text)):
 #         for j in range(i,len(text)):
 #             yield text[i:j+1]
 
 def all_variants(text):
     for size in range(len(text)):
-        for i in range(len(text)-size):
-            yield text[i:i+size+1]
+        for i in range(len(text) - size):
+            yield text[i:i + size + 1]
+
 
 a = all_variants("abc")
 for i in a:
