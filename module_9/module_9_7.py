6e5cdd25e52d9a999a8fcc82f49f110234b2bc8c
Yed Makarov <myed@list.ru>
2024-10-10 21:01:03 +0500

    Urban-University homeworks

--- module_9/module_9_7.py
+++ module_9/module_9_7.py
@@ -3,9 +3,15 @@
 def is_prime(func):
     def wrapper(*args):
         result = func(*args)
-        return result
+
+        if result <= 1:
+            return f"Ни простое ни составное \n{result}"
+        for i in range(2, int(result ** 0.5) + 1):
+            if result % i == 0:
+                return f"Составное \n{result}"
+        return f"Простое \n{result}"
+
     return wrapper
-    # return func
 
 
 @is_prime
@@ -20,5 +26,9 @@ def sum_three(*args: int):
 # Main
 result = sum_three(2, 3, 6)
 print(result)
+
 result = sum_three(2, 3, 7)
+print(result)
+
+result = sum_three(1, 0, 0)
 print(result)
