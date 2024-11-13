25d46361ce475f97042eee587b132df8f35134a5
Yed Makarov <myed@list.ru>
2024-11-13 19:00:23 +0500

    Urban-University homeworks

--- module_12/tests_12_1.py
+++ module_12/tests_12_1.py
@@ -8,18 +8,18 @@ import runner
 class RunnerTest(unittest.TestCase):
     def test_walk(self):
         tw1 = runner.Runner('runner_walk')
-        print(tw1.name)
+        # print(tw1.name)
         for _ in range(0, 10):
             tw1.walk()
-        print(tw1.distance)
+        # print(tw1.distance)
         self.assertEqual(tw1.distance, 50)
 
     def test_run(self):
         tr1 = runner.Runner('runner_run')
-        print(tr1.name)
+        # print(tr1.name)
         for _ in range(0, 10):
             tr1.run()
-        print(tr1.distance)
+        # print(tr1.distance)
         self.assertEqual(tr1.distance, 100)
 
     def test_challenge(self):
