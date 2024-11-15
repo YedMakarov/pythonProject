0348c9d26139466a5d388672a2eda567629c5aeb
Yed Makarov <myed@list.ru>
2024-11-15 07:23:09 +0500

    Urban-University homeworks

--- module_12/tests_12_2.py
+++ module_12/tests_12_2.py
@@ -3,51 +3,69 @@
 
 
 import unittest
-import runner_and_tournament
+import runner_and_tournament as rat
 
 
 class TournamentTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.all_results = {}
+        cls.all_results1 = {}
+        cls.all_results2 = {}
+        cls.all_results3 = {}
 
     def setUp(self):
-        self.r1 = runner_and_tournament.Runner('Усэйн', 10)
-        self.r2 = runner_and_tournament.Runner('Андрей', 9)
-        self.r3 = runner_and_tournament.Runner('Ник', 3)
+        self.r1 = rat.Runner('Усэйн', 10)
+        self.r2 = rat.Runner('Андрей', 9)
+        self.r3 = rat.Runner('Ник', 3)
 
     @classmethod
     def tearDownClass(cls):
-        for key in sorted(cls.all_results.keys()):
-            print(f"{key}: {cls.all_results[key]}")
+        all_results = {}
+        for key in sorted(cls.all_results1.keys()):
+            all_results[key] = f"{cls.all_results1[key]}"
+        print(all_results)
+
+        all_results = {}
+        for key in sorted(cls.all_results2.keys()):
+            all_results[key] = f"{cls.all_results2[key]}"
+        print(all_results)
+
+        all_results = {}
+        for key in sorted(cls.all_results3.keys()):
+            all_results[key] = f"{cls.all_results3[key]}"
+        print(all_results)
+
 
     def test_race_usain_and_nick(self):
-        tournament = runner_and_tournament.Tournament(90, self.r1, self.r3)
+        tournament = rat.Tournament(90, self.r1, self.r3)
         results = tournament.start()
         self.all_results.update(results)
         # print(results)
+
+        self.all_results1.update(results)
 
         last_runner_name = max(results.keys())
         self.assertTrue(results[last_runner_name] == "Ник")
 
     def test_race_andrey_and_nick(self):
-        tournament = runner_and_tournament.Tournament(90, self.r2, self.r3)
+        tournament = rat.Tournament(90, self.r2, self.r3)
         results = tournament.start()
         self.all_results.update(results)
         # print(results)
+
+        self.all_results2.update(results)
 
         last_runner_name = max(results.keys())
         self.assertTrue(results[last_runner_name] == "Ник")
 
     def test_race_usain_andrey_and_nick(self):
-        tournament = runner_and_tournament.Tournament(90, self.r1, self.r2, self.r3)
+        tournament = rat.Tournament(90, self.r1, self.r2, self.r3)
         results = tournament.start()
         self.all_results.update(results)
         # print(results)
 
+        self.all_results3.update(results)
+
         last_runner_name = max(results.keys())
         self.assertTrue(results[last_runner_name] == "Ник")
-
-
-if __name__ == "__main__":
-    unittest.main()
