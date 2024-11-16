5bb98af3f32a043ac530052d6757163f16ae3de5
Yed Makarov <myed@list.ru>
2024-11-16 22:16:17 +0500

    Urban-University homeworks

---
+++ module_12/runner_test.py
@@ -0,0 +1,43 @@
+# Для tests_12_4
+
+
+import unittest
+import rt_with_exceptions as rtwe
+
+
+class RunnerTest(unittest.TestCase):
+    is_frozen = False
+
+    @unittest.skipIf(is_frozen, "Тесты в этом кейсе заморожены")
+    def test_walk(self):
+        try:
+            logging.info(f"Sucsessuful")
+            tw1 = runner.Runner('runner_walk')
+            # print(tw1.name)
+            for _ in range(0, 10):
+                tw1.walk()
+            # print(tw1.distance)
+            self.assertEqual(tw1.distance, 50)
+        except
+
+    @unittest.skipIf(is_frozen, "Тесты в этом кейсе заморожены")
+    def test_run(self):
+        tr1 = runner.Runner('runner_run')
+        # print(tr1.name)
+        for _ in range(0, 10):
+            tr1.run()
+        # print(tr1.distance)
+        self.assertEqual(tr1.distance, 100)
+
+    @unittest.skipIf(is_frozen, "Тесты в этом кейсе заморожены")
+    def test_challenge(self):
+        tw2 = runner.Runner('runner_walk')
+        tr2 = runner.Runner('runner_run')
+        for _ in range(0, 10):
+            tw2.walk()
+            tr2.run()
+        self.assertNotEqual(tw2.distance, tr2.distance)
+
+
+if __name__ == "__main__":
+    unittest.main()
