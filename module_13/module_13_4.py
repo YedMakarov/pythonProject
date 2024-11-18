7560b59875241bdd58f665af94dc0a2eeae3a5ad
Yed Makarov <myed@list.ru>
2024-11-18 22:47:13 +0500

    Urban-University homeworks

---
+++ module_13/module_13_4.py
@@ -0,0 +1,60 @@
+# Задача "Цепочка вопросов".
+
+
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
+import asyncio
+
+api = ""
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+
+@dp.message_handler(text="Calories")
+async def set_age(message):
+    await message.answer("Введите свой возраст:")
+    await UserState.age.set()
+
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message, state):
+    await state.update_data(msg_age=message.text)
+    await message.answer("Введите свой рост:")
+    await UserState.growth.set()
+
+
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message, state):
+    await state.update_data(msg_growth=message.text)
+    await message.answer("Введите свой вес:")
+    await UserState.weight.set()
+
+
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message, state):
+    await state.update_data(msg_weight=message.text)
+    data = await state.get_data()
+    print(f"data - {data}")
+
+    calories = (10 * int(data['msg_weight']) + 6.25 * int(data['msg_growth'])
+                - 5 * int(data['msg_age']) + 5)
+
+    await message.answer(f"Доступное количество петребления килокалорий - {calories}.")
+    await state.finish()
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
+
+"""
+Упрощенный вариант формулы Миффлина-Сан Жеора:
+для мужчин: 10 х вес (кг) + 6,25 x рост (см) – 5 х возраст (г) + 5;
+"""
