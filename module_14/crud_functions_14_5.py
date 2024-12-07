d0ab68f3b25dd32320b8a7924a8170d428e4e916
Yed Makarov <myed@list.ru>
2024-12-07 23:34:22 +0500

    Urban-University homeworks

---
+++ module_14/crud_functions_14_5.py
@@ -0,0 +1,71 @@
+# Для задачи "Продуктовая база".
+import sqlite3
+
+
+def initiate_db():
+    connection = sqlite3.connect("products_14_5.db")
+    cursor = connection.cursor()
+
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Products(
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL,
+    description TEXT,
+    price INTEGER NOT NULL
+    );
+    ''')
+
+    # Создание таблицы Users
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Users(
+    id INTEGER PRIMARY KEY,
+    username TEXT NOT NULL,
+    email TEXT  NOT NULL,
+    age INTEGER NOT NULL,
+    balance INTEGER NOT NULL DEFAULT 1000
+    );
+    ''')
+
+    cursor.execute(" INSERT INTO Products (title, description, price) VALUES (?,?,?)",
+                   ("Продукт 1", "Описание 1", "100"))
+    cursor.execute(" INSERT INTO Products (title, description, price) VALUES (?,?,?)",
+                   ("Продукт 2", "Описание 2", "200"))
+    cursor.execute(" INSERT INTO Products (title, description, price) VALUES (?,?,?)",
+                   ("Продукт 3", "Описание 3", "300"))
+    cursor.execute(" INSERT INTO Products (title, description, price) VALUES (?,?,?)",
+                   ("Продукт 4", "Описание 4", "400"))
+
+    connection.commit()
+    connection.close()
+
+
+def get_all_products():
+    connection = sqlite3.connect("products_14_5.db")
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT * FROM Products")
+    results = cursor.fetchall()
+    connection.close()
+
+    return results
+
+
+
+# Новая функция
+def add_user(username: str, email: str, age: int):
+    connection = sqlite3.connect("products_14_5.db")
+    cursor = connection.cursor()
+
+    cursor.execute(" INSERT INTO Users (username, email, age) VALUES (?,?,?)",
+                   ("Продукт 1", "Описание 1", "100"))
+
+    connection.commit()
+    connection.close()
+
+
+# Новая функция
+def is_included(username: str):
+    connection = sqlite3.connect("products_14_5.db")
+    cursor = connection.cursor()
+
+    connection.close()
