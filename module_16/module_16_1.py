94d2bfc06046e7828a95f2080e3afc4fd16892a2
Yed Makarov <myed@list.ru>
2024-12-21 02:10:23 +0500

    Urban-University homeworks

--- module_16/module_16_1.py
+++ module_16/module_16_1.py
@@ -11,26 +11,26 @@ app = FastAPI()  # создаем экземпляр приложения чер
 # функция-обработчик корневого GET-запроса вместе с методом-декоратором app.get
 @app.get("/")
 # async def read_root() -> dict:
-async def read_root():
-    return {"Главная страница"}
+async def read_root() -> str:
+    return "Главная страница"
 
 
 # функция-обработчик импровизированного GET-запроса страницы вместе с методом-декоратором app.get
 @app.get("/user/admin")
-async def read_admin_page():
-    return {"Вы вошли как администратор"}
+async def read_admin_page() -> str:
+    return "Вы вошли как администратор"
 
 
 # функция-обработчик импровизированного GET-запроса пользователя вместе с методом-декоратором app.get
 @app.get("/user/{user_id}")
-async def read_user_page(user_id: int):
-    return {f"Вы вошли как пользователь № {user_id}"}
+async def read_user_page(user_id: int) -> str:
+    return f"Вы вошли как пользователь № {user_id}"
 
 
-# функция-обработчик импровизированного GET-запроса пользователя вместе и его возраста с методом-декоратором app.get
-@app.get("/user/{user_name}/{age}")
-async def read_user_page2(user_name: str, age: int):
-    return {f"Информация о пользователе. Имя: {user_name}, Возраст: {age}"}
+# функция-обработчик импровизированного GET-запроса пользователя и его возраста вместе с методом-декоратором app.get
+@app.get("/user")
+async def read_users_page(username: str, age: int) -> str:
+    return f"Информация о пользователе. Имя: {username}, Возраст: {age}"
 
 # C:\Users\Yed\PycharmProjects\pythonProject\module_16> python -m uvicorn module_16_1:app
-# uvicorn main:app --reload
+# uvicorn main:app --reload | uvicorn module_16_1:app --reload
