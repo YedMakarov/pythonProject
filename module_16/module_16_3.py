ccf01e00a4b72a72558fe261e22516b91bdbca94
Yed Makarov <myed@list.ru>
2024-12-21 12:05:43 +0500

    Urban-University homeworks

--- module_16/module_16_3.py
+++ module_16/module_16_3.py
@@ -1,6 +1,7 @@
 # Задача "Имитация работы с БД".
 
 from fastapi import FastAPI, Path
+from typing import Annotated
 
 app = FastAPI()  # создаем экземпляр приложения через конструктор
 
@@ -22,7 +23,7 @@ async def read_users() -> dict:
 
 # Функция-обработчик GET-запроса регистрирующая нового пользователя
 @app.post("/user/{username}/{age}")
-async def add_user(username: str = Path(min_length=5, max_length=20, description="Enter username"),
+async def add_user(username: Annotated[str, Path(min_length=5, max_length=20, description="Enter username")],
                    age: int = Path(ge=18, le=120, description="Enter age'")) -> str:
     current_index = str(int(max(users, key=int)) + 1)
     users[current_index] = f"Имя: {username}, возраст: {age}"
@@ -31,8 +32,8 @@ async def add_user(username: str = Path(min_length=5, max_length=20, description
 
 # Функция-обработчик GET-запроса обновляющая данные зарегистрированного пользователя
 @app.put("/user/{user_id}/{username}/{age}")
-async def update_user(user_id: int = Path(ge=1, le=100, description="Enter User ID"),
-                      username: str = Path(min_length=5, max_length=20, description="Enter username"),
+async def update_user(user_id: Annotated[int, Path(ge=1, le=100, description="Enter User ID")],
+                      username: Annotated[str, Path(min_length=5, max_length=20, description="Enter username")],
                       age: int = Path(ge=18, le=120, description="Enter age'")) -> str:
     users[str(user_id)] = f"Имя: {username}, возраст: {age}"
     return f"User {user_id} has been updated."
