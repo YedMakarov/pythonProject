0ae394cabbfb9a4cb7219344edfec958efd5bd5e
Yed Makarov <myed@list.ru>
2024-12-23 09:35:34 +0500

    Urban-University homeworks

--- module_16/module_16_4.py
+++ module_16/module_16_4.py
@@ -1,7 +1,7 @@
 # Задача "Модель пользователя".
 
 from fastapi import FastAPI, Path, status, Body, HTTPException
-# from typing import Annotated
+
 from pydantic import BaseModel
 from typing import List
 
@@ -12,47 +12,52 @@ users = []
 
 
 class User(BaseModel):
-    id: int = None
-    username: str = None
-    age: int = None
+    id: int
+    username: str
+    age: int
 
 
 # Функция-обработчик GET-запроса выдающая зарегистрированных пользователей
-@app.get("/users")
-async def read_users() -> List[User]:
+@app.get("/users", response_model=List[User])
+async def get_users() -> List[User]:
     return users
 
 
-# # Функция-обработчик GET-запроса регистрирующая нового пользователя
-@app.post("/user/{username}/{age}")
-def add_user(user_: User, username: str, age: int) -> str:
-    user_.id = len(users) + 1
-    user_.username = username
-    user_.age = age
-    users.append(user_)
-    return f"User with name {user_.username} and age {user_.age} year(s) is created."
+# # Функция-обработчик POST-запроса регистрирующая нового пользователя
+@app.post("/user/{username}/{age}", response_model=User)
+def add_user(username: str, age: int):
+    user_id = (users[-1].id + 1) if users else 1
+    print(user_id)
+    new_user = User(id=user_id, username=username, age=age)
+    users.append(new_user)
+    return new_user
 
 
-# Функция-обработчик GET-запроса обновляющая данные зарегистрированного пользователя
-@app.put("/user/{user_id}/{username}/{age}")
-async def update_user(user_: User, user_id: int, username: str, age: int) -> str:
-    try:
-        users[user_id].username = user_.username
-        users[user_id].age = user_.age
-        return f"User with ID update name to {user_.username} and age to {user_.age} year(s)."
-    except IndexError:
-        raise HTTPException(status_code=404, detail=f"User with ID {user_.id} is not found")
+# Функция-обработчик PUT-запроса обновляющая данные зарегистрированного пользователя
+@app.put("/user/{user_id}/{username}/{age}", response_model=User)
+def update_user(user_id: int, username: str, age: int):
+    for user in users:
+        if user.id == user_id:
+            user.username = username
+            user.age = age
+            return user
+    raise HTTPException(status_code=404, detail=f"User with ID {user_id} was not found")
 
 
-# Функция-обработчик GET-запроса удаляющая зарегистрированного пользователя по его id
-@app.delete("/user/{user_id}")
-async def remove_user(user_: User, user_id: int) -> str:
-    try:
-        user_.id = user_id
-        users.pop(user_.id)
-        return f"User with ID {user_.id} is deleted."
-    except IndexError:
-        raise HTTPException(status_code=404, detail=f"User with ID {user_.id} is not found")
+# Функция-обработчик DELETE-запроса удаляющая зарегистрированного пользователя по его id
+@app.delete("/user/{user_id}", response_model=User)
+def delete_user(user_id: int):
+    for index, user in enumerate(users):
+        if user.id == user_id:
+            return users.pop(index)
+    raise HTTPException(status_code=404, detail=f"User with ID {user_id} was not found")
 
-# C:\Users\Yed\PycharmProjects\pythonProject\module_16> python -m uvicorn module_16_4:app
-# uvicorn main:app --reload | uvicorn module_16_4:app --reload
+
+# Main
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="127.0.0.1", port=8000)
+
+# C:\Users\...\PycharmProjects\pythonProject\module_16> python -m uvicorn module_16_4:app
+# uvicorn main:app --reload
