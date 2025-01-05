61783e4f5eb434cb33581189dd2183d5584eea68
Yed Makarov <myed@list.ru>
2025-01-05 23:53:55 +0500

    Urban-University homeworks

--- module_16/module_16_5.py
+++ module_16/module_16_5.py
@@ -1,11 +1,17 @@
 # Задача "Список пользователей в шаблоне".
 
-from fastapi import FastAPI, Path, status, Body, HTTPException
+from fastapi import FastAPI, Path, status, Body, HTTPException, Request
 from typing import Annotated
 from pydantic import BaseModel
 from typing import List
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 
-app = FastAPI()  # создаем экземпляр приложения через конструктор
+# app = FastAPI()  # создаем экземпляр приложения через конструктор
+app = FastAPI(swagger_ui_parameters={"tryItOutEnabled": True}, debug=True)
+
+# Настраиваем папку с шаблонами
+templates = Jinja2Templates(directory="templates")
 
 # Создаём импровизированную базу данных на основе пустого списка
 users = []
@@ -17,10 +23,25 @@ class User(BaseModel):
     age: int
 
 
-# Функция-обработчик GET-запроса выдающая зарегистрированных пользователей
-@app.get("/users", response_model=List[User])
-async def get_users() -> List[User]:
-    return users
+# Инициализация пользователей
+# users.append(User(id=1, username="UrbanUser ", age=24))
+# users.append(User(id=2, username="UrbanTest", age=22))
+# users.append(User(id=3, username="Capybara", age=60))
+
+
+# Функция-обработчик GET-запроса главной страницы
+@app.get("/", response_class=HTMLResponse)
+async def get_main_page(request: Request):
+    return templates.TemplateResponse("users.html", {"request": request, "users": users})
+
+
+# Функция-обработчик GET-запроса выдающая зарегистрированных пользователей + Jinja2
+@app.get("/user/{user_id}", response_class=HTMLResponse)
+async def get_user(request: Request, user_id: int):
+    for user in users:
+        if user.id == user_id:
+            return templates.TemplateResponse("users.html", {"request": request, "user": user})
+    raise HTTPException(status_code=404, detail="User  not found")
 
 
 # # Функция-обработчик POST-запроса регистрирующая нового пользователя
@@ -28,7 +49,6 @@ async def get_users() -> List[User]:
 def add_user(username: Annotated[str, Path(min_length=5, max_length=20, description="Enter username")],
              age: int = Path(ge=18, le=120, description="Enter age'")):
     user_id = (users[-1].id + 1) if users else 1
-    print(user_id)
     new_user = User(id=user_id, username=username, age=age)
     users.append(new_user)
     return new_user
